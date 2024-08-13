15b7b5947fcf5ee04dbd14332be70ea5e6c797b4
onyyyyx <shadwangel404@proton.me>
2024-08-13 23:15:31 +0200

    Update

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ body.style.background = "#0c0c0c"
 document.addEventListener('mousemove', (e) => {
   const x = Math.floor(e.clientX + window.pageXOffset);
   const y = Math.floor(e.clientY + window.pageYOffset);
-  const gradient = `radial-gradient(at ${x}px ${y}px, #0c0c0c, #1b1b1b)`;
+  const gradient = `radial-gradient(at ${x}px ${y}px, #1b1b1b, #0c0c0c)`;
   body.style.backgroundImage = gradient;
 });
